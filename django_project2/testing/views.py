a296b58e125f16ead3c553fd407ea9de78efd92f
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-07-13 15:30:59 -0400

    Finished the 8th lesson

--- django_project2/testing/views.py
+++ django_project2/testing/views.py
@@ -1,6 +1,9 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     # View function for home page of site.
@@ -35,3 +38,16 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    # Generic class-based view listing books on loan to current user.
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower = self.request.user)
+            .filter(status__exact = 'o')
+            .order_by('due_back')
+            )
