1a12c37cd6768e735f722232a9407f2e85d4a046
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-07-18 18:52:17 -0400

    Let's stop here for now.
    While the URLs for changing the authors are here, I need the accounts to test them.

--- django_project2/testing/views.py
+++ django_project2/testing/views.py
@@ -12,6 +12,11 @@ from django.urls import reverse
 
 from testing.forms import RenewBookForm
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from testing.models import Author
+
 
 def index(request):
     # View function for home page of site.
@@ -94,3 +99,16 @@ def renew_book_librarian(request, pk):
         'book_instance': book_instance,
     }
     return render(request, 'testing/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
